a1667532df0540fae2320cb712e246c6e21da692
Rafael Domingues <rafael.domingues@confirm8.com>
2025-03-12 17:23:27 -0300

    feat: implements store

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,1 +1,50 @@
-export function createStore() {}
+import { useSyncExternalStore } from 'react';
+
+type SetterFn<T> = (prevState: T) => Partial<T>;
+type SetStateFn<T> = (partialState: Partial<T> | SetterFn<T>) => void;
+
+export function createStore<TState>(
+  createState: (setState: SetStateFn<TState>, getState: () => TState) => TState,
+) {
+  let state: TState;
+  let listeners: Set<VoidFunction>;
+
+  function notifyListeners() {
+    listeners.forEach((listener) => listener());
+  }
+
+  function setState(partialState: Partial<TState> | SetterFn<TState>) {
+    const newValue =
+      typeof partialState === 'function' ? partialState(state) : partialState;
+
+    state = {
+      ...state,
+      ...newValue,
+    };
+
+    notifyListeners();
+  }
+
+  function subscribe(listener: VoidFunction) {
+    listeners.add(listener);
+
+    return () => {
+      listeners.delete(listener);
+    };
+  }
+
+  function getState() {
+    return state;
+  }
+
+  function useStore<TValue>(
+    selector: (currentState: TState) => TValue,
+  ): TValue {
+    return useSyncExternalStore(subscribe, () => selector(state));
+  }
+
+  state = createState(setState, getState);
+  listeners = new Set<VoidFunction>();
+
+  return useStore;
+}
